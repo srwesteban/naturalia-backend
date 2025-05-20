d43b5e808a1955b91a22ca6154fddd2a98860794
William Esteban <sr.w@hotmail.com>
2025-05-20 13:24:45 -0500

    Initial commit - Spring Boot base project

---
+++ src/main/java/com/naturalia/backend/NaturaliaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.naturalia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturaliaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturaliaBackendApplication.class, args);
+	}
+
+}
